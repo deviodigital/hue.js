f3aee6c2c9ebf714407ff9cb3e7ccc7eaa474732
Robert DeVore <deviodigital@gmail.com>
2018-03-26 04:04:35 -0400

    👌 IMPROVE: recalculated square dimensions for full color range
    Thanks [@benjaminmueller](https://github.com/Automattic/Iris/pull/48)

--- dist/hue.js
+++ dist/hue.js
@@ -771,8 +771,8 @@
 
 			control = this.controls.squareDrag;
 			dimensions = {
-				w: square.width(),
-				h: square.height()
+				w: square.width() - control.width(),
+				h: square.height() - control.height()
 			};
 			square.data( 'dimensions', dimensions );
 			return dimensions;
